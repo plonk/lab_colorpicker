f0bba304659f5f6946e70a87cb4d82ae86bc4def
Yoteichi <plonk@piano.email.nejp>
2014-11-08 05:20:29 +0900

    first commit

---
+++ color.js
@@ -0,0 +1,193 @@
+// 色変換の数式は http://www.easyrgb.com/index.php?X=MATH を元にしている。
+function rgb_to_xyz(rgb) {
+    var R = rgb.r / 255;
+    var G = rgb.g / 255;
+    var B = rgb.b / 255;
+
+    var f = function(x) {
+        if (x > 0.04045)
+            return Math.pow((x + 0.055) / 1.055, 2.4);
+        else
+            return x / 12.92;
+    };
+
+    R = f(R) * 100;
+    G = f(G) * 100;
+    B = f(B) * 100;
+
+    // Observer. = 2°, Illuminant = D65
+    return { x: R * 0.4124 + G * 0.3576 + B * 0.1805,
+             y: R * 0.2126 + G * 0.7152 + B * 0.0722,
+             z: R * 0.0193 + G * 0.1192 + B * 0.9505 };
+}
+
+function lab_to_xyz(lab) {
+    var y = (lab.l + 16) / 116;
+    var x = lab.a / 500 + y;
+    var z = y - lab.b / 200;
+
+    var f = function (g) { 
+        if (Math.pow(g,3) > 0.008856)
+            return Math.pow(g,3);
+        else
+            return (g - 16/116) / 7.787;
+    };
+
+    var refX =  95.047;
+    var refY = 100.000;
+    var refZ = 108.883;
+    return { x: refX * f(x), y: refY * f(y), z: refZ * f(z) };
+}
+
+function xyz_to_rgb(xyz) {
+    var var_X = xyz.x / 100;
+    var var_Y = xyz.y / 100;
+    var var_Z = xyz.z / 100;
+
+    var var_R = var_X *  3.2406 + var_Y * -1.5372 + var_Z * -0.4986;
+    var var_G = var_X * -0.9689 + var_Y *  1.8758 + var_Z *  0.0415;
+    var var_B = var_X *  0.0557 + var_Y * -0.2040 + var_Z *  1.0570;
+
+    if (var_R > 0.0031308) {
+        var_R = 1.055 * Math.pow(var_R, 1/2.4) - 0.055;
+    } else {
+        var_R = 12.92 * var_R;
+    } 
+    if (var_G > 0.0031308) {
+        var_G = 1.055 * Math.pow(var_G, 1/2.4) - 0.055;
+    } else {
+        var_G = 12.92 * var_G;
+    }
+    if (var_B > 0.0031308) {
+        var_B = 1.055 * Math.pow(var_B, 1/2.4) - 0.055;
+    } else {
+        var_B = 12.92 * var_B;
+    }
+    return { r: var_R * 255, g: var_G * 255, b: var_B * 255 };
+}
+
+function xyz_to_lab(xyz) {
+    //Observer= 2°, Illuminant= D65
+    var X = xyz.x /  95.047;
+    var Y = xyz.y / 100.000;
+    var Z = xyz.z / 108.883;
+
+    var f = function (x) {
+        if (x > 0.008856)
+            return Math.pow(x, 1/3);
+        else
+            return 7.787 * x + 16 / 116;
+    };
+
+    return { l: (116 * f(Y)) - 16,
+             a: 500 * (f(X) - f(Y)),
+             b: 200 * (f(Y) - f(Z)) };
+}
+
+function lab_to_rgb(lab) {
+    var xyz = lab_to_xyz(lab);
+    // console.log(xyz);
+    return xyz_to_rgb(xyz);
+}
+
+function rgb_to_lab(rgb) {
+    var xyz = rgb_to_xyz(rgb);
+    return xyz_to_lab(xyz);
+}
+
+function rgb_in_range(rgb) {
+    var inrange = function (x) { 
+        return x >= 0 && x <= 255;
+    };
+
+    return !!(inrange(Math.round(rgb.r)) &&
+              inrange(Math.round(rgb.g)) &&
+              inrange(Math.round(rgb.b)));
+}
+
+function rgb_to_string(rgb) {
+    if (rgb_in_range(rgb))
+        return "rgb("+Math.round(rgb.r)+","+Math.round(rgb.g)+","+Math.round(rgb.b)+")";
+    else
+        return "rgb(128,128,128)";
+}
+
+function rgb_to_string_clipped(rgb) {
+    var clip = function (x) { 
+        if (x < 0) return 0; else if (x > 255) return 255; else return x;
+    };
+
+    return "rgb("+clip(Math.round(rgb.r))+","+clip(Math.round(rgb.g))+","+clip(Math.round(rgb.b))+")";
+}
+
+
+function lab_rotate(lab, radian) {
+    var theta = lab_hue(lab);
+    var d = distance_origin(lab.a, lab.b)
+
+    return { l: lab.l,
+             a: Math.cos(theta + radian) * d,
+             b: Math.sin(theta + radian) * d };
+}
+
+function lab_hue_degree(lab) {
+    return Math.round((lab_hue_radian(lab) / Math.PI * 180 + 360) % 360);
+}
+
+function lab_hue_radian(lab) {
+    return Math.atan2(lab.b, lab.a);
+}
+
+// 補色を得る
+function lab_complement(lab) {
+    return lab_rotate(lab, Math.PI);
+}
+
+function lab_tetrade(order, lab) {
+    return lab_rotate(lab, Math.PI * 2 / 3 * order);
+}
+
+function lab_is_real(lab) {
+    return rgb_in_range(lab_to_rgb(lab));
+}
+
+function desaturate(pos) {
+    var i = 0;
+    var alpha;
+
+    for (i = 0; i <= 100; i++) {
+        alpha = i * 0.01;
+        var c = { l: pos.l,
+                  a: 0 * alpha + (1-alpha) * pos.a,
+                  b: 0 * alpha + (1-alpha) * pos.b };
+
+        if (lab_is_real(c)) {
+            return c;
+        }
+    }
+    alert("desaturate: logic error");
+}
+
+function adjustl(pos) {
+    if (lab_is_real(pos)) return pos;
+
+    // l, a, b 
+    var l, c;
+
+    // l を上げてみる
+    for (l = pos.l; l <= 100; l++) {
+        c = { l: l, a: pos.a, b: pos.b};
+        if (lab_is_real(c))
+            return c;
+    }
+
+    // l を下げてみる
+    for (l = pos.l; l >= 0; l -= 1) {
+        c = { l: l, a: pos.a, b: pos.b};
+        if (lab_is_real(c))
+            return c;
+    }
+
+    // 元の値を返す
+    return pos;
+}
---
+++ script.js
@@ -0,0 +1,364 @@
+// アプリケーションオブジェクト
+App = {}
+
+
+window.onload = function() {
+    initialize_app();
+
+    redraw(); // 最初の描画
+}
+
+function initialize_app() {
+    App.ctl = {};
+
+    // Canvas とコンテキストを用意する
+    App.canvas = document.getElementById('plane');
+    if (!App.canvas || !App.canvas.getContext) return false;
+    App.ctx = App.canvas.getContext('2d');
+
+    // App.canvas.addEventListener('mousemove', function(evt) {
+    //     var mousePos = getMousePos(App.canvas, evt);
+    //     var p = canvas_to_ab(mousePos);
+    //     App.target = { l: App.target.l, a: p.a, b: p.b };
+    //     redraw();
+    // }, false);
+
+    App.canvas.onclick = on_canvas_click;
+
+    App.ctl.lightness_bar = document.getElementById('lightness_bar');
+
+    App.ctl.lightness_bar.addEventListener('click', on_lightness_bar_click, false);
+
+
+    App.ctl.color = document.getElementById('color');
+    App.ctl.color.oninput = on_color_input;
+
+    App.ctl.l = document.getElementById('l');
+    App.ctl.a = document.getElementById('a');
+    App.ctl.b = document.getElementById('b');
+
+    App.ctl.l.oninput = on_lab_input;
+    App.ctl.a.oninput = on_lab_input;
+    App.ctl.b.oninput = on_lab_input;
+
+    App.ctl.rgb_r = document.getElementById('rgb_r');
+    App.ctl.rgb_g = document.getElementById('rgb_g');
+    App.ctl.rgb_b = document.getElementById('rgb_b');
+
+    App.ctl.rgb_r.oninput = on_rgb_input;
+    App.ctl.rgb_g.oninput = on_rgb_input;
+    App.ctl.rgb_b.oninput = on_rgb_input;
+
+    App.ctl.hue = document.getElementById('hue');
+    App.ctl.hue.oninput = on_hue_input;
+
+    App.ctl.sample = document.getElementById('target_sample');
+
+    App.ctl.desaturate = document.getElementById('desaturate');
+    App.ctl.desaturate.onclick = on_desaturate_click;
+    App.ctl.adjustl = document.getElementById('adjustl');
+    App.ctl.adjustl.onclick = on_adjustl_click;
+
+    App.target = { l: 50, a: 0, b: 0 };
+}
+
+function on_hue_input() {
+    var angle = +App.ctl.hue.value;
+    if (angle < 0 || angle > 360) return false;
+
+    var theta = angle / 180 * Math.PI;
+    var d = distance_origin(App.target.a, App.target.b);
+    App.target = { l: App.target.l,
+             a: Math.cos(theta) * d,
+             b: Math.sin(theta) * d };
+    redraw();
+}
+
+function on_rgb_input() {
+    console.log("input");
+    var r = +App.ctl.rgb_r.value;
+    var g = +App.ctl.rgb_g.value;
+    var b = +App.ctl.rgb_b.value;
+
+    App.target = rgb_to_lab({ r: r, g: g, b: b });
+    redraw();
+}
+
+function on_canvas_click(event) {
+    var pos = getMousePos(App.canvas, event);
+
+    ab = canvas_to_ab(pos);
+    App.target = { l: App.target.l, a: ab.a, b: ab.b };
+    redraw();
+}
+
+function on_desaturate_click() {
+    App.target = desaturate(App.target);
+    redraw();
+}
+
+function on_adjustl_click() {
+    App.target = adjustl(App.target);
+    redraw();
+}
+
+function on_lightness_bar_click(event) {
+    var pos = getMousePos(App.ctl.lightness_bar, event);
+
+    var l = Math.round(pos.x / App.ctl.lightness_bar.width * 100);
+
+    App.target = { l: l, a: App.target.a, b: App.target.b };
+    redraw();
+}
+
+function on_lab_input() {
+    App.target = { l: Number(App.ctl.l.value),
+                   a: Number(App.ctl.a.value),
+                   b: Number(App.ctl.b.value) };
+    redraw();
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function on_color_input() {
+    var rgb = parse_color(App.ctl.color.value);
+    if (rgb) {
+        App.target = rgb_to_lab(rgb);
+        redraw();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function canvas_to_ab(p) {
+    var w = Number(App.canvas.width);
+    var h = Number(App.canvas.height);
+    var xnotch = w/256, ynotch = h/256;
+
+    // Y 軸は上が+
+    return { a: -128 + (p.x / xnotch),
+             b:  128 - (p.y / ynotch) };
+}
+
+function lab_to_canvas(lab) {
+    var w = Number(App.canvas.width);
+    var h = Number(App.canvas.height);
+    var x = (lab.a + 128) / 256 * w;
+    var y = h - ((lab.b + 128) / 256 * h);
+    
+    return { x: x, y: y };
+}
+
+function draw_crosshair(context, lab_color, style) {
+    var radius = 5;
+    var coords = lab_to_canvas(lab_color);
+    var x = coords.x, y = coords.y;
+
+    context.beginPath();
+
+    context.lineWidth = 2;
+    context.strokeStyle = style;
+
+    context.moveTo(x, y - 4);
+    context.lineTo(x, y - 9);
+    context.stroke();    
+
+    context.moveTo(x, y + 4);
+    context.lineTo(x, y + 9);
+    context.stroke();    
+
+    context.moveTo(x - 4, y);
+    context.lineTo(x - 9, y);
+    context.stroke();    
+
+    context.moveTo(x + 4, y);
+    context.lineTo(x + 9, y);
+    context.stroke();
+
+    context.closePath();
+}
+
+function draw_plane(ctx, width, height, lightness) {
+    ctx.fillStyle = "rgb(128,128,128)";
+    ctx.fillRect(0, 0, width, height);
+    draw_color_slice(ctx,  width, height, lightness);
+    draw_crosshair(ctx, App.target, "#fff");
+}
+
+function draw_color_slice(ctx, width, height, lightness) {
+    var x, y;
+    var lab;
+    var state; // before: 0, in: 1, out: 2
+    var rgb;
+
+    for (y = 0; y < height; y++) {
+        inner:
+        for (x = 0; x < width; x++) {
+            state = 0;
+
+            lab = canvas_to_ab({ x: x, y: y });
+            rgb = lab_to_rgb({ l: lightness, a: lab.a, b: lab.b });
+
+            switch (state) {
+            case 0:
+                if (rgb_in_range(rgb)) {
+                    ctx.fillStyle = rgb_to_string(rgb); 
+                    ctx.fillRect(x, y, 1, 1);
+                    state = 1;
+                }
+                break;
+            case 1:
+                if (rgb_in_range(rgb)) {
+                    ctx.fillStyle = rgb_to_string(rgb); 
+                    ctx.fillRect(x, y, 1, 1);
+                } else {
+                    state = 2;
+                }
+                break;
+            case 2:
+                break inner;
+            }
+        }
+    }
+}
+
+function parse_color(value) {
+    var m;
+   
+    if (m = value.match(/^#(.)(.)(.)$/)) {
+        return { r: parseInt("0x"+m[1]) / 15 * 255,
+                 g: parseInt("0x"+m[2]) / 15 * 255,
+                 b: parseInt("0x"+m[3]) / 15 * 255 };
+    } else if (m = value.match(/^#(..)(..)(..)$/)) {
+        return { r: parseInt("0x"+m[1]),
+                 g: parseInt("0x"+m[2]),
+                 b: parseInt("0x"+m[3]) };
+    } else {
+        return null;
+    }
+}
+
+function fill_sample(name, lab) {
+    cs = document.getElementById(name);
+    cs.style.backgroundColor = rgb_to_string(lab_to_rgb(lab));
+    cs.textContent = rgb_to_string(lab_to_rgb(lab));
+
+    csc = document.getElementById(name+"_clipped");
+    csc.style.backgroundColor = rgb_to_string_clipped(lab_to_rgb(lab));
+    csc.textContent = rgb_to_string_clipped(lab_to_rgb(lab));
+}
+
+function fill_sample() {
+    App.ctl.sample.style.backgroundColor = rgb_to_string(lab_to_rgb(App.target));
+}
+
+function distance_origin(x, y) {
+    return Math.sqrt(x * x + y * y);
+}
+
+
+function redraw() {
+    var width, height;
+
+    if (!App.target) return false;
+ 
+    width = Number(App.canvas.width);
+    height = Number(App.canvas.height);
+
+    draw_plane(App.ctx, width, height, App.target.l);
+
+    update_controls();
+}
+
+function str_repeat(phrase, n) {
+    var res = "";
+
+    while (n >= 0) {
+        res += phrase;
+        n--;
+    }
+    return res;
+}
+
+function pad_zero(str, n) {
+    if (str.length >= n) {
+        return str;
+    } else {
+        return str_repeat('0', n - str.length);
+    }
+}
+
+function to_hex(rgb) {
+    var hex = [ rgb.r, rgb.g, rgb.b ].map(function (f) {
+        return pad_zero(Math.round(f).toString(16), 2);
+    }).join("");
+
+    return "#" + hex;
+}
+
+function update_controls() {
+    var rgb;
+
+    App.ctl.l.value = Math.round(App.target.l);
+    App.ctl.a.value = Math.round(App.target.a);
+    App.ctl.b.value = Math.round(App.target.b);
+
+    rgb = lab_to_rgb( App.target );
+    App.ctl.rgb_r.value = Math.round(rgb.r);
+    App.ctl.rgb_g.value = Math.round(rgb.g);
+    App.ctl.rgb_b.value = Math.round(rgb.b);
+
+    App.ctl.hue.value = lab_hue_degree(App.target);
+
+    if (lab_is_real(App.target)) {
+        App.ctl.color.value = to_hex(lab_to_rgb(App.target));
+    } else {
+        App.ctl.color.value = "n/a";
+    }
+
+    update_lightness_bar();
+
+    fill_sample();
+}
+
+function update_lightness_bar() {
+    var ctx = App.ctl.lightness_bar.getContext('2d');
+    var w = Number( App.ctl.lightness_bar.width );
+    var h = Number( App.ctl.lightness_bar.height );
+    var xnotch = w / 100;
+    var l;
+    
+    ctx.fillStyle = "#888";
+    ctx.fillRect(0, 0, w, h);
+
+    for (l = 0; l <= 100; l += 1) {
+        ctx.fillStyle = rgb_to_string(
+            lab_to_rgb({ l: l, a: App.target.a, b: App.target.b })
+        );
+        ctx.fillRect(l * xnotch - xnotch/2, 0, xnotch, h);
+    }
+
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#fff";
+    ctx.moveTo(App.target.l * xnotch, 0);
+    ctx.lineTo(App.target.l * xnotch, App.ctl.lightness_bar.height / 2);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#000";
+    ctx.moveTo(App.target.l * xnotch, App.ctl.lightness_bar.height / 2);
+    ctx.lineTo(App.target.l * xnotch, App.ctl.lightness_bar.height);
+    ctx.stroke();
+
+    ctx.closePath();
+}
